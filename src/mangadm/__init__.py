82092dd60b440d4506253edaa7f0963e5b92667d
xMohnad <mohnad00967@gmail.com>
2025-07-13 15:25:14 +0000

    refactor: lazily import MangaDM and FormatType to improve CLI startup time

--- src/mangadm/__init__.py
+++ src/mangadm/__init__.py
@@ -1,1 +1,24 @@
-from .core.mangadm import FormatType, MangaDM
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from .components.types import FormatType as FormatTypeType
+    from .core.mangadm import MangaDM as MangaDMType
+
+MangaDM: "type[MangaDMType]"
+FormatType: "type[FormatTypeType]"
+
+__all__ = ["MangaDM", "FormatType"]
+
+
+def __getattr__(name: str) -> "Any":
+    if name == "MangaDM":
+        from .core.mangadm import MangaDM
+
+        return MangaDM
+    if name == "FormatType":
+        from .components.types import FormatType
+
+        return FormatType
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
