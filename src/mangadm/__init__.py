ad4f5ebd8e3b34c364d5ada98a4cdd7d7616c9d9
xMohnad <mohnad00967@gmail.com>
2025-10-13 19:52:23 +0000

    build(pyproject): migrate from setuptools to uv_build and set static version

--- src/mangadm/__init__.py
+++ src/mangadm/__init__.py
@@ -1,7 +1,8 @@
+from importlib.metadata import version
 from pathlib import Path
 
 import typer
 
-__version__ = "0.6.0"
+__version__ = version(__name__)
 __config__: Path = Path(typer.get_app_dir(__name__)) / "config.yaml"
 __config__.parent.mkdir(parents=True, exist_ok=True)
